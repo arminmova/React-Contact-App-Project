0e1ad5bbb93e39533842e4f71e7f1a30b9984a53
Armin Movahedi <arminmova99@gmail.com>
2024-11-20 17:53:11 +0100

    version0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+import Contact from "./components/Contact"
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Contact />
+
+    </>
+  )
+}
+
+export default App
